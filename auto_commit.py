94c7b2fe719b5eedea57873c41d1baeabb8914c6
Shashwat Tandon <111636617+killlercram@users.noreply.github.com>
2025-10-11 11:37:53 +0530

    Create auto_commit.py

---
+++ auto_commit.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import os
+from datetime import datetime
+import subprocess
+
+#----Configuration----
+REPO_PATH = "."
+
+# Navigating to the repo
+os.chdir(REPO_PATH)
+
+#updating or creating a file
+with open("log.txt, "a") as f:
+    f.write(f"Automated commit on {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+
+#git operations
+try: 
+    subprocess.run(["git", "add", "."], check=True)
+    subprocess.run(["git", "commit", "-m", f"Automated commit on {datetime.now().strftime('%Y-%m-%d')}"], check=True)
+    subprocess.run(["git", "push"], check=True)
+    print("commited and pushed successful!")
+except subprocess.CalledProcessError:
+    print("No Changes to commit or puush failed")
